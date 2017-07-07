c14597416b883e825bd5fb473f3bc90af0ef07bc
bush2582 <bush2582@163.com>
2017-07-07 23:50:42 +0800

    tmp

---
+++ src/action/login.py
@@ -0,0 +1,7 @@
+# _*_ coding:utf-8 _*_
+from flask import Blueprint, render_template,request,session,redirect
+
+login = Blueprint('login', __name__)
+@login.route('/')
+def LoginPageInit():
+    return render_template('login.html')
